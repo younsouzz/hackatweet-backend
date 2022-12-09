3c7a15149190aace42650d447e1a32962fbdcbe0
Youness <y.ouzzedi@hotmail.com>
2022-12-09 16:43:33 +0100

    first commit

---
+++ routes/users.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const bcrypt = require('bcrypt');
+const uid2 = require('uid2');
+
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['firstName', 'username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Check if the user has not already been registered
+  User.findOne({ username: { $regex: new RegExp(req.body.username, 'i') } }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        firstName: req.body.firstName,
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+        canBookmark: true,
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
+});
+
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ username: { $regex: new RegExp(req.body.username, 'i') } }).then(data => {
+    if (bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token, username: data.username, firstName: data.firstName });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
+});
+
+module.exports = router;
