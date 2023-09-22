9eb132fabdfa3c85dfae8be5d0045e26ccd0a8bb
Youness <youns@outlook.ie>
2023-09-22 17:53:25 +0200

    Remove logging

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.CONNECTION_STRING;
 
-console.log('connection string is:'+connectionString);
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
